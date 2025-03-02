9378cb372269ab80539f26651b22884ef55688fa
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-02 12:56:58 +0530

    resolved the issue of api-gateway

--- user-service/user-service/src/main/java/com/company/TradingPlatform/user_service/controller/AuthController.java
+++ user-service/user-service/src/main/java/com/company/TradingPlatform/user_service/controller/AuthController.java
@@ -1,4 +1,66 @@
 package com.company.TradingPlatform.user_service.controller;
 
+
+import com.company.TradingPlatform.user_service.dtos.AuthRequestDto;
+import com.company.TradingPlatform.user_service.dtos.UserDto;
+import com.company.TradingPlatform.user_service.exceptions.BadRequestException;
+import com.company.TradingPlatform.user_service.service.AuthService;
+import com.company.TradingPlatform.user_service.service.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Slf4j
 public class AuthController {
+
+    private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@RequestBody AuthRequestDto authRequestDto){
+        try{
+            UserDto userDto = authService.signup(authRequestDto);
+            return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+        }catch(BadRequestException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Signup failed: "+e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequestDto authRequestDto){
+        try{
+            String token = authService.login(authRequestDto);
+            return ResponseEntity.ok(token);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Login failed: "+e.getMessage());
+        }
+    }
+
+    @GetMapping("/validate-token")
+    public Boolean validateToken(@RequestParam("token") String token){
+        return jwtTokenProvider.validateToken(token);
+    }
+
+    @GetMapping("/user/{email}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email){
+        UserDto userDto = authService.getUserByEmail(email);
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<UserDto> updateUser(@RequestBody Long userId){
+        log.info("Received request tp fetch user by ID:{}",userId);
+        UserDto userDto = authService.getUserById(userId);
+        log.info("Returning user; {}",userDto);
+        return ResponseEntity.ok(userDto);
+    }
 }
