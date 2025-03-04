7453579f4289cedbc8960db4b7c3c3355aabf187
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-04 10:44:42 +0530

    added controller layer of the wallet Service

--- wallet-service/wallet-service/src/main/java/com/company/TradingPlatform/wallet_service/controller/WalletController.java
+++ wallet-service/wallet-service/src/main/java/com/company/TradingPlatform/wallet_service/controller/WalletController.java
@@ -2,14 +2,13 @@ package com.company.TradingPlatform.wallet_service.controller;
 
 
 import com.company.TradingPlatform.wallet_service.Service.WalletService;
+import com.company.TradingPlatform.wallet_service.dtos.TransactionRequest;
+import com.company.TradingPlatform.wallet_service.dtos.TransactionResponse;
 import com.company.TradingPlatform.wallet_service.dtos.WalletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wallet")
@@ -24,6 +23,31 @@ public class WalletController {
         log.info("Creating new wallet for userId: {}",userId);
         WalletResponse response = walletService.createWallet(userId);
         log.info("Wallet created successfully for userId: {}",userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<WalletResponse> getWallet(@PathVariable Long userId){
+        log.info("Fetching wallet details for userId: {}",userId);
+        WalletResponse response = walletService.getWallet(userId);
+        log.info("Wallet details retrieved successfully for userId: {}",userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/deposit")
+    public ResponseEntity<TransactionResponse> deposit(@RequestBody TransactionRequest request){
+        log.info("Processing deposit for userId: {} with amount: {}",request.getUserId(),request.getAmount());
+        TransactionResponse response = walletService.deposit(request);
+        log.info("Deposit successful for userId: {},",request.getUserId());
+        log.info("Deposit successful for userId: {}",request.getUserId());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<TransactionResponse> withdraw(@RequestBody TransactionRequest request){
+        log.info("Processing withdrawal for userId: {} with amount: {}",request.getUserId(),request.getAmount());
+        TransactionResponse response = walletService.withdraw(request);
+        log.info("Withdrawal successful for userId: {},",request.getUserId());
         return ResponseEntity.ok(response);
     }
 }
