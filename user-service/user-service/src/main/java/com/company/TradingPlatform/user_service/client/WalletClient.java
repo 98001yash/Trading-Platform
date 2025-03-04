fc580beb2d48d674ff493bd5adce3f1676ec2597
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-04 12:48:34 +0530

    added the order-service in the application

--- user-service/user-service/src/main/java/com/company/TradingPlatform/user_service/client/WalletClient.java
+++ user-service/user-service/src/main/java/com/company/TradingPlatform/user_service/client/WalletClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "wallet-service", url = "http://localhost:9030")  // Correct port
+@FeignClient(name = "wallet-service", url = "http://localhost:9030")
 public interface WalletClient {
 
-    @PostMapping("/api/wallets/create/{userId}")  // Fixed Path Variable syntax
+    @PostMapping("/api/wallets/create/{userId}")
     void createWallet(@PathVariable Long userId);
 }
